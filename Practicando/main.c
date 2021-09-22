b837a88985df121d5928184c4bc42dc7626b21df
martinico83 <martin.nicolas.os83@gmail.com>
2021-09-22 19:31:16 -0300

    Uso de fgets

--- Practicando/main.c
+++ Practicando/main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    printf("Hello world!\n");
-    printf("HOLA DON PEPITO"\n);
+    //arreglar ya que no puedo ingresar nombres con espacios
+    char titulo[30];
+    char nombre[30];
+
+    printf("Ingrese el titulo del libro: ");
+
+    fgets(titulo,sizeof(titulo),stdin);
+    titulo[strlen(titulo)-1]='\0';
+
+    printf("Ingrese el nombre del autor: ");
+
+    fgets(nombre,sizeof(nombre),stdin);
+    nombre[strlen(nombre)-1]='\0';
+
+    printf("El titulo del libro es: %s y el autor se llama %s",titulo,nombre);
+
     return 0;
 }
